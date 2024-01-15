72d2f252d6ac00a317b97b953590a458c9d15628
Kushagr Shukla <kushagrshukla2409@gmail.com>
2024-01-15 19:26:41 +0530

    protecting routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -15,5 +15,9 @@ router.post('/login', loginController);
 // test route
 router.get('/test', requireSignIn, isAdmin, testController)
 
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
 export default router;
 
