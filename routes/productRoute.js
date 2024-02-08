a98d17ceb8523e577ed6b031b46ceb898d52975d
Kushagr Shukla <kushagrshukla2409@gmail.com>
2024-02-08 09:42:23 +0530

    Added products

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -24,6 +24,6 @@ router.get("/get-product/:slug", getSingleProductController)
 router.get("/product-photo/:pid", productPhotoController)
 
 //delete produc
-router.delete("/product/:pid", deleteProductController)
+router.delete("/delete-product/:pid", deleteProductController)
 
 export default router;
